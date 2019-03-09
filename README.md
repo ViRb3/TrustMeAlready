832a7657c9d959ef8dec5aabfd978643f4edb3a9
Victor <virb3e@gmail.com>
2019-03-09 19:43:48 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## TrustMeAlready 
+An Xposed module to disable SSL verification and pinning on Android using the excellent technique provided by [Mattia Vinci](https://techblog.mediaservice.net/2018/11/universal-android-ssl-check-bypass-2/)
+
+### Requirements
+* An Xposed-compatible hooking system. Some options at time of writing:
+    * [EdXposed](https://github.com/ElderDrivers/EdXposed) (Android 9)
+    * ['OG' Xposed](https://forum.xda-developers.com/showthread.php?t=3034811) (Android 5-8)
+
+### Tested
+* Android 9.0, arm64
+* EdXposed v0.3.0.0_beta3
---
+++ app/src/main/java/com/virb3/trustme/TrustMeAlready.java
@@ -0,0 +1,42 @@
+package com.virb3.trustme;
+
+import java.lang.reflect.Method;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedBridge;
+
+import static de.robv.android.xposed.XposedHelpers.*;
+
+public class TrustMeAlready implements IXposedHookZygoteInit {
+
+    private static final String SSL_CLASS_NAME = "com.android.org.conscrypt.TrustManagerImpl";
+    private static final String SSL_METHOD_NAME = "checkTrustedRecursive";
+
+    @Override
+    public void initZygote(StartupParam startupParam) throws Throwable {
+         XposedBridge.log("TrustMeAlready loaded!");
+
+         for (Method method : findClass(SSL_CLASS_NAME, null).getDeclaredMethods()) {
+             if (!method.getName().equals(SSL_METHOD_NAME)) {
+                 continue;
+             }
+
+             List<Object> params = new ArrayList<>();
+             params.addAll(Arrays.asList(method.getParameterTypes()));
+             params.add(new XC_MethodReplacement() {
+                 @Override
+                 protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                     return new ArrayList<X509Certificate>();
+                 }
+             });
+
+             XposedBridge.log("Hooking method:\n"+method.toString());
+             findAndHookMethod(SSL_CLASS_NAME, null, SSL_METHOD_NAME, params.toArray());
+         }
+    }
+}
