9e4347418d50059d878488da263e6457151a930c
Victor <virb3e@gmail.com>
2019-03-10 22:47:06 +0000

    Better check if hooking correct method

--- app/src/main/java/com/virb3/trustmealready/Main.java
+++ app/src/main/java/com/virb3/trustmealready/Main.java
@@ -1,6 +1,8 @@
 package com.virb3.trustmealready;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,24 +21,42 @@ public class Main implements IXposedHookZygoteInit {
 
     @Override
     public void initZygote(StartupParam startupParam) throws Throwable {
-         XposedBridge.log("TrustMeAlready loaded!");
+        XposedBridge.log("TrustMeAlready loaded!");
 
-         for (Method method : findClass(SSL_CLASS_NAME, null).getDeclaredMethods()) {
-             if (!method.getName().equals(SSL_METHOD_NAME)) {
-                 continue;
-             }
+        for (Method method : findClass(SSL_CLASS_NAME, null).getDeclaredMethods()) {
+            if (!checkSSLMethod(method)) {
+                continue;
+            }
 
-             List<Object> params = new ArrayList<>();
-             params.addAll(Arrays.asList(method.getParameterTypes()));
-             params.add(new XC_MethodReplacement() {
-                 @Override
-                 protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-                     return new ArrayList<X509Certificate>();
-                 }
-             });
+            List<Object> params = new ArrayList<>();
+            params.addAll(Arrays.asList(method.getParameterTypes()));
+            params.add(new XC_MethodReplacement() {
+                @Override
+                protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                    return new ArrayList<X509Certificate>();
+                }
+            });
 
-             XposedBridge.log("Hooking method:\n"+method.toString());
-             findAndHookMethod(SSL_CLASS_NAME, null, SSL_METHOD_NAME, params.toArray());
-         }
+            XposedBridge.log("Hooking method:\n" + method.toString());
+            findAndHookMethod(SSL_CLASS_NAME, null, SSL_METHOD_NAME, params.toArray());
+        }
+    }
+
+    private boolean checkSSLMethod(Method method) {
+        if (!method.getName().equals(SSL_METHOD_NAME)) {
+            return false;
+        }
+
+        Type returnType = method.getGenericReturnType();
+        if (!(returnType instanceof ParameterizedType)) {
+            return false;
+        }
+
+        Type[] args = ((ParameterizedType) returnType).getActualTypeArguments();
+        if (args.length != 1 || args[0] instanceof X509Certificate) {
+            return false;
+        }
+
+        return true;
     }
 }
